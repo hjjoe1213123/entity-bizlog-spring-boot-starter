16899c5ea84a35b301a9894295909b138259d577
hjjoe1213123 <hjjoe1213123@163.com>
2021-04-12 12:03:43 +0800

    init

---
+++ src/main/java/cn/idugou/bizlog/BizlogApplication.java
@@ -0,0 +1,13 @@
+package cn.idugou.bizlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/idugou/bizlog/BizlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.idugou.bizlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
