52f79cff18c11de2e1fad0cd73eea22cab63d8a7
hjjoe1213123 <hjjoe1213123@163.com>
2021-04-13 15:59:59 +0800

    init

--- src/main/java/cn/idugou/bizlog/EntityLogAutoConfiguration.java
+++ src/main/java/cn/idugou/bizlog/EntityLogAutoConfiguration.java
@@ -1,10 +1,13 @@
 package cn.idugou.bizlog;
 
+import cn.idugou.bizlog.listener.EntityLogService;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -26,4 +29,22 @@ public class EntityLogAutoConfiguration implements ApplicationContextAware {
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException { appCtx = applicationContext; }
 
     public static ApplicationContext getApplicationContext() { return appCtx; }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EntityLogScanService entityLogScanService() { return new EntityLogScanService(appCtx); }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EntityLogService entityLogService() { return new EntityLogService(); }
+
+    @Bean
+    public EntityLogAspectj entityLogAspectj() {
+        EntityLogAspectj entityLogAspectj = new EntityLogAspectj();
+        entityLogAspectj.setOrder(entityLogProperties.getAopOrder());
+        return entityLogAspectj;
+    }
+
+    @Bean
+    public EntityLogSessionService entityLogSessionService() { return new EntityLogSessionService(); }
 }
